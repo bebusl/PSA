abd1bd2e4c53e69c99e35a9fdd94d6736e67ba1c
dungbik <q1w2e30630@gmail.com>
2021-06-05 00:58:42 +0900

    [수정] : consumer 이름 수정(crawl->result)

--- server/kafka/consumer.js
+++ server/kafka/consumer.js
@@ -2,20 +2,20 @@ const { Kafka } = require("kafkajs");
 
 const {
   KAFKA_BOOTSTRAP_SERVER,
-  CRAWL_CLINET_ID,
-  CRAWL_GROUP_ID,
-  CRAWL_TOPIC,
+  RESULT_CLINET_ID,
+  RESULT_GROUP_ID,
+  RESULT_TOPIC,
 } = require("../env");
 
 const kafka = new Kafka({
   brokers: [KAFKA_BOOTSTRAP_SERVER],
-  clientId: CRAWL_CLINET_ID,
+  clientId: RESULT_CLINET_ID,
 });
 
 const init = async () => {
-  const consumer = kafka.consumer({ groupId: CRAWL_GROUP_ID });
+  const consumer = kafka.consumer({ groupId: RESULT_GROUP_ID });
   await consumer.connect();
-  await consumer.subscribe({ topic: CRAWL_TOPIC });
+  await consumer.subscribe({ topic: RESULT_TOPIC });
   consumer.run({
     eachMessage: async ({ topic, partition, message }) => {
       console.log(
