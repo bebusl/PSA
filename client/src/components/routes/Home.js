26b0ff84ec0249c20a58a161be6281e1fe92fe5d
dungbik <q1w2e30630@gmail.com>
2021-06-09 00:57:50 +0900

    [추가] : 키워드 검색 테스트 기능 추가

--- client/src/components/routes/Home.js
+++ client/src/components/routes/Home.js
@@ -9,25 +9,23 @@ let socket;
 function Home({ history }) {
   const { values, onChange } = useInput({ searchItem: "" });
 
-  if (socket == null) socket = socketIOClient("http://localhost:5000");
+  if (socket == null) {
+    socket = socketIOClient("http://localhost:5000");
+    socket.on("keywords", (keywords) => {
+      console.log(keywords);
+      history.push({
+        pathname: "/keyword",
+        state: { keywords: keywords },
+      });
+    });
+  }
 
   const onSubmit = (e) => {
     e.preventDefault(); //새로고침 안함.
     console.log("소켓으로 넣을 값:", values.searchItem); //확인하고
 
     socket.emit("send message", { searchItem: values.searchItem });
-    const keywords = [
-      "품질",
-      "키워드2",
-      "키워드3",
-      "키워드4",
-      "키워드5",
-      "키워드6",
-      "키워드7",
-      "키워드8",
-      "키워드9",
-    ];
-    console.log("history", history);
+
     // history.push({
     //   pathname: "/keyword",
     //   state: { keywords: keywords },
