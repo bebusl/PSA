1c6d828ff3af3f16585246d0c44bc9043d62f6f4
bebusl <bebus1998@naver.com>
2021-09-14 16:21:35 +0900

    [수정] : 회원가입/로그인 관련 state 다룰 수 있도록 redux 업데이트

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,25 +1,22 @@
 import { LOGIN, LOGOFF, UPDATE_USERDATA } from "./action";
 import { UPDATE_LIKEKEYWORD, UPDATE_HATEKEYWORD } from "./action";
 
-const reducer = (
-  state = { isLogin: false, userData: {}, likeKwd: [], hateKwd: [] },
-  action
-) => {
-  switch (action.type) {
-    case LOGOFF:
-      return { ...state, isLogin: false, userData: {} };
-    case LOGIN:
-      return { ...state, isLogin: true, userData: action.userData };
-    case UPDATE_USERDATA:
-      return { ...state, userData: action.userData };
-    case UPDATE_LIKEKEYWORD:
-      return { ...state, likeKwd: action.keywords };
-    case UPDATE_HATEKEYWORD:
-      return { ...state, hateKwd: action.keywords };
+const reducer = (state = { isLogin: false, userData: {}, likeKwd: [], hateKwd: [] }, action) => {
+    switch (action.type) {
+        case LOGOFF:
+            return { ...state, isLogin: false, userData: {} };
+        case LOGIN:
+            return { ...state, isLogin: true, userData: action.userData };
+        case UPDATE_USERDATA:
+            return { ...state, userData: action.userData };
+        case UPDATE_LIKEKEYWORD:
+            return { ...state, likeKwd: action.keywords };
+        case UPDATE_HATEKEYWORD:
+            return { ...state, hateKwd: action.keywords };
 
-    default:
-      return state;
-  }
+        default:
+            return state;
+    }
 };
 
 export default reducer;
