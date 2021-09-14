efc73817fafa37b9f3601f2813342a68d301fe69
bebusl <bebus1998@naver.com>
2021-09-14 16:13:03 +0900

    [추가] : 서버 passport 모듈 추가 및 환경설정, 적용
    - passport는 인증 편하게 해주는 모듈
    - 회원가입/로그인 관련 라우터도 추가 ./auth

--- server/app.js
+++ server/app.js
@@ -1,10 +1,32 @@
+const COOKIE_SECRET = require("./env").COOKIE_SECRET;
 const express = require("express");
-const app = express();
+const cookieParser = require("cookie-parser");
+const passport = require("passport");
 const cors = require("cors");
+const userRouter = require("./routes/user");
+
+const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cors());
-app.get("/", (req, res) => res.send("Hello World!"));
+app.use(cookieParser(COOKIE_SECRET));
+app.use(passport.initialize());
+require("./passport/config")(passport);
+
+app.use("/auth", userRouter);
+
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
+    res.status(500);
+    res.send("500", err.message);
+});
 
 module.exports = app;
