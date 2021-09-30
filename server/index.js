5efbb782a072b111e49aeb96bf2bbec646db0b05
bebusl <bebus1998@naver.com>
2021-09-30 17:43:46 +0900

    [수정] : 선택할 키워드 구분해서 socket으로 전송
    - 기존에는 상품에서 나온 키워드 모두를 일괄적으로 보내서 거기서
      선호/불호 키워드를 선택하도록 했음.
    - 사욪자가 전체 키워드 중에서 선호/불호 키워드를 선택했는데 선호/불호가
      표시된 리뷰가 없다면, 랭킹 알고리즘 연산에 있어서 오류를 유발함
     - 따라서 선호 키워드를 선택할 때는 선호 리뷰가 있는 키워드만 보내고,
       불호 키워드를 선택할 때는 불호 리뷰가 있는 키둬드를 보내서 랭킹
       알고리즘에 오류가 발생하지 않도록 함.

--- server/index.js
+++ server/index.js
@@ -5,204 +5,211 @@ const SERVER_PORT = require("./env").SERVER_PORT;
 const { TestCollection, productdetails, analyses } = require("./models");
 
 function dotp(x, y) {
-  function dotp_sum(a, b) {
-    return a + b;
-  }
-  function dotp_times(a, i) {
-    return x[i] * y[i];
-  }
-  return x.map(dotp_times).reduce(dotp_sum, 0);
+    function dotp_sum(a, b) {
+        return a + b;
+    }
+    function dotp_times(a, i) {
+        return x[i] * y[i];
+    }
+    return x.map(dotp_times).reduce(dotp_sum, 0);
 }
 
 function cosineSimilarity(A, B) {
-  var similarity =
-    dotp(A, B) /
-    ((Math.sqrt(dotp(A, A)) + 0.00000001) *
-      (Math.sqrt(dotp(B, B)) + 0.00000001));
-  return similarity;
+    var similarity = dotp(A, B) / ((Math.sqrt(dotp(A, A)) + 0.00000001) * (Math.sqrt(dotp(B, B)) + 0.00000001));
+    return similarity;
 }
 
 io.on("connection", function (socket) {
-  console.log("connected", socket.id);
+    console.log("connected", socket.id);
 
-  crawl_producer.init();
-  result_consumer.init();
+    crawl_producer.init();
+    result_consumer.init();
 
-  socket.on("send message", ({ searchItem }) => {
-    const msg = searchItem;
-    TestCollection.findOne({ keyword: msg })
-      .then(async (keyword) => {
-        if (keyword) {
-          const { products } = keyword;
-          let keywords = {};
-          for (productRef in products) {
-            const product = await productdetails.findById(
-              products[productRef]["oid"]
-            );
-            const analysis = await analyses.findById(product.analysis["oid"]);
-            const result = analysis.result;
-            const key = Object.keys(result[0]);
-            const values = Object.values(result[0]);
-            for (let idx = 0; idx < key.length; idx++) {
-              let value = values[idx]["POS"];
-              if (key[idx] in keywords) {
-                keywords[key[idx]] += value;
-              } else {
-                keywords[key[idx]] = value;
-              }
-            }
-          }
-          result = [];
-          for (key in keywords) {
-            temp = {};
-            temp.keyword = key;
-            temp.count = keywords[key];
-            result.push(temp);
-          }
+    socket.on("send message", ({ searchItem }) => {
+        const msg = searchItem;
+        TestCollection.findOne({ keyword: msg })
+            .then(async (keyword) => {
+                if (keyword) {
+                    const { products } = keyword;
+                    let poskeywords = {};
+                    let negkeywords = {};
+                    for (productRef in products) {
+                        const product = await productdetails.findById(products[productRef]["oid"]);
+                        const analysis = await analyses.findById(product.analysis["oid"]);
+                        const result = analysis.result;
+                        const key = Object.keys(result[0]);
+                        const values = Object.values(result[0]);
+                        for (let idx = 0; idx < key.length; idx++) {
+                            let posvalue = values[idx]["POS"];
+                            let negvalue = values[idx]["NEG"];
 
-          result.sort(function (a, b) {
-            return b.count - a.count;
-          });
+                            if (key[idx] in poskeywords) {
+                                poskeywords[key[idx]] += posvalue;
+                            } else {
+                                poskeywords[key[idx]] = posvalue;
+                            } //키워드에 대한 긍정적 키워드 수 카운트
 
-          real = result.map((dict) => {
-            return dict.keyword;
-          });
+                            if (key[idx] in negkeywords) {
+                                negkeywords[key[idx]] += negvalue;
+                            } else {
+                                negkeywords[key[idx]] = negvalue;
+                            } //키워드에 대한 부정적 키워드 수 카운트
+                        }
+                    }
+                    let posresult = [];
+                    let negresult = [];
+                    for (key in poskeywords) {
+                        temp = {};
+                        temp.keyword = key;
+                        temp.count = poskeywords[key];
+                        if (temp.count !== 0) {
+                            posresult.push(temp);
+                        }
+                    }
 
-          io.emit("keywords", real);
-        } else {
-          crawl_producer.sendMessage(msg);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-        // crawl_producer.sendMessage(msg);
-      });
-  });
+                    for (key in negkeywords) {
+                        temp = {};
+                        temp.keyword = key;
+                        temp.count = negkeywords[key];
+                        if (temp.count !== 0) {
+                            negresult.push(temp);
+                        }
+                    }
 
-  socket.on("selected keywords", function ({ searchItem, likeword, hateword }) {
-    TestCollection.findOne({ keyword: searchItem })
-      .then(async (keyword) => {
-        if (keyword) {
-          const { products } = keyword;
-          let productlist = [];
-          const keywords = [];
-          for (productRef in products) {
-            let product = await productdetails.findById(
-              products[productRef]["oid"]
-            );
-            const analy = await analyses.findById(product.analysis["oid"]);
-            const allKeywords = Object.keys(analy["result"][0]);
-            const posKeywords = likeword.filter(
-              (keyword) =>
-                allKeywords.includes(keyword) &&
-                analy["result"][0][keyword]["POS"] >
-                  analy["result"][0][keyword]["NEG"]
-            );
-            const negKeywords = hateword.filter(
-              (keyword) =>
-                allKeywords.includes(keyword) &&
-                analy["result"][0][keyword]["POS"] <
-                  analy["result"][0][keyword]["NEG"]
-            );
-            allKeywords.map((keyword) => {
-              if (!keywords.includes(keyword)) keywords.push(keyword);
-            });
+                    posresult.sort(function (a, b) {
+                        return b.count - a.count;
+                    });
 
-            const { _id, imageUrl, name, price, url } = product._doc;
-            productlist.push({
-              _id,
-              name,
-              price,
-              imageUrl,
-              url,
-              posKeywords,
-              negKeywords,
-              allKeywords: analy["result"][0],
-            });
-          }
+                    negresult.sort(function (a, b) {
+                        return b.count - a.count;
+                    });
 
-          good_vec = [];
-          bad_vec = [];
+                    posreal = posresult.map((dict) => dict.keyword);
+                    negreal = negresult.map((dict) => dict.keyword);
 
-          keywords.map((keyword) => {
-            good_vec.push(likeword.includes(keyword) ? 1 : 0);
-            bad_vec.push(hateword.includes(keyword) ? 1 : 0);
-          });
-
-          for (product_ in productlist) {
-            product = productlist[product_];
-            const { allKeywords } = product;
-            if (allKeywords) {
-              const allKeywords_ = Object.keys(allKeywords);
-              product_good_vec = [];
-              product_bad_vec = [];
-              for (keyword_ in keywords) {
-                keyword = keywords[keyword_];
-                if (
-                  allKeywords_.includes(keyword) &&
-                  likeword.includes(keyword)
-                ) {
-                  if (
-                    allKeywords[keyword]["POS"] == 0 ||
-                    allKeywords[keyword]["POS"] <= allKeywords[keyword]["NEG"]
-                  ) {
-                    product_good_vec.push(0);
-                  } else {
-                    product_good_vec.push(
-                      allKeywords[keyword]["POS"] /
-                        (allKeywords[keyword]["POS"] +
-                          allKeywords[keyword]["NEG"])
-                    );
-                  }
+                    io.emit("keywords", posreal, negreal);
                 } else {
-                  product_good_vec.push(0);
+                    crawl_producer.sendMessage(msg);
                 }
+            })
+            .catch((err) => {
+                console.log(err);
+                // crawl_producer.sendMessage(msg);
+            });
+    });
 
-                if (
-                  allKeywords_.includes(keyword) &&
-                  hateword.includes(keyword)
-                ) {
-                  if (
-                    allKeywords[keyword]["NEG"] == 0 ||
-                    allKeywords[keyword]["NEG"] <= allKeywords[keyword]["POSS"]
-                  ) {
-                    product_bad_vec.push(0);
-                  } else {
-                    product_bad_vec.push(
-                      allKeywords[keyword]["NEG"] /
-                        (allKeywords[keyword]["POS"] +
-                          allKeywords[keyword]["NEG"])
-                    );
-                  }
-                } else {
-                  product_bad_vec.push(0);
+    socket.on("selected keywords", function ({ searchItem, likeword, hateword }) {
+        TestCollection.findOne({ keyword: searchItem })
+            .then(async (keyword) => {
+                if (keyword) {
+                    const { products } = keyword;
+                    let productlist = [];
+                    const keywords = [];
+                    for (productRef in products) {
+                        let product = await productdetails.findById(products[productRef]["oid"]);
+                        const analy = await analyses.findById(product.analysis["oid"]);
+                        const allKeywords = Object.keys(analy["result"][0]);
+                        const posKeywords = likeword.filter(
+                            (keyword) =>
+                                allKeywords.includes(keyword) &&
+                                analy["result"][0][keyword]["POS"] > analy["result"][0][keyword]["NEG"]
+                        );
+                        const negKeywords = hateword.filter(
+                            (keyword) =>
+                                allKeywords.includes(keyword) &&
+                                analy["result"][0][keyword]["POS"] < analy["result"][0][keyword]["NEG"]
+                        );
+                        allKeywords.map((keyword) => {
+                            if (!keywords.includes(keyword)) keywords.push(keyword);
+                        });
+
+                        const { _id, imageUrl, name, price, url } = product._doc;
+                        productlist.push({
+                            _id,
+                            name,
+                            price,
+                            imageUrl,
+                            url,
+                            posKeywords,
+                            negKeywords,
+                            allKeywords: analy["result"][0],
+                        });
+                    }
+
+                    good_vec = [];
+                    bad_vec = [];
+
+                    keywords.map((keyword) => {
+                        good_vec.push(likeword.includes(keyword) ? 1 : 0);
+                        bad_vec.push(hateword.includes(keyword) ? 1 : 0);
+                    });
+
+                    for (product_ in productlist) {
+                        product = productlist[product_];
+                        const { allKeywords } = product;
+                        if (allKeywords) {
+                            const allKeywords_ = Object.keys(allKeywords);
+                            product_good_vec = [];
+                            product_bad_vec = [];
+                            for (keyword_ in keywords) {
+                                keyword = keywords[keyword_];
+                                if (allKeywords_.includes(keyword) && likeword.includes(keyword)) {
+                                    if (
+                                        allKeywords[keyword]["POS"] == 0 ||
+                                        allKeywords[keyword]["POS"] <= allKeywords[keyword]["NEG"]
+                                    ) {
+                                        product_good_vec.push(0);
+                                    } else {
+                                        product_good_vec.push(
+                                            allKeywords[keyword]["POS"] /
+                                                (allKeywords[keyword]["POS"] + allKeywords[keyword]["NEG"])
+                                        );
+                                    }
+                                } else {
+                                    crawl_producer.sendMessage(msg);
+                                }
+
+                                if (allKeywords_.includes(keyword) && hateword.includes(keyword)) {
+                                    if (
+                                        allKeywords[keyword]["NEG"] == 0 ||
+                                        allKeywords[keyword]["NEG"] <= allKeywords[keyword]["POS"]
+                                    ) {
+                                        product_bad_vec.push(0);
+                                    } else {
+                                        product_bad_vec.push(
+                                            allKeywords[keyword]["NEG"] /
+                                                (allKeywords[keyword]["POS"] + allKeywords[keyword]["NEG"])
+                                        );
+                                    }
+                                } else {
+                                    product_bad_vec.push(0);
+                                }
+                            }
+
+                            good_sim = cosineSimilarity(good_vec, product_good_vec);
+                            bad_sim = cosineSimilarity(bad_vec, product_bad_vec);
+
+                            product["sim"] = good_sim - bad_sim;
+                        }
+                    }
+
+                    productlist.sort(function (a, b) {
+                        return b.sim - a.sim;
+                    });
+
+                    io.emit("productlist", productlist);
                 }
-              }
+            })
+            .catch((err) => console.log(err));
+    });
 
-              good_sim = cosineSimilarity(good_vec, product_good_vec);
-              bad_sim = cosineSimilarity(bad_vec, product_bad_vec);
-
-              product["sim"] = good_sim - bad_sim;
-            }
-          }
-
-          productlist.sort(function (a, b) {
-            return b.sim - a.sim;
-          });
-
-          io.emit("productlist", productlist);
-        }
-      })
-      .catch((err) => console.log(err));
-  });
-
-  socket.on("disconnect", function () {
-    console.log("disconnected", socket.id);
-  });
+    socket.on("disconnect", function () {
+        console.log("disconnected", socket.id);
+    });
 });
 
 server.listen(+SERVER_PORT);
 server.on("listening", () => {
-  const addr = server.address();
-  console.log(`Server running on ${addr.address}:${addr.port}`);
+    const addr = server.address();
+    console.log(`Server running on ${addr.address}:${addr.port}`);
 });
