30f523cb3c5814dadc46ab9627b5e551a9b6a976
bebusl <bebus1998@naver.com>
2022-09-06 22:37:40 +0900

    refactor: redux store 정의

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,40 @@
-import { createStore } from "redux";
-import reducer from "./reducer";
-import { composeWithDevTools } from "redux-devtools-extension";
+import {
+  configureStore,
+  getDefaultMiddleware,
+  combineReducers,
+} from "@reduxjs/toolkit";
+import authReducer from "./slice/authSlice";
+import keywordReducer from "./slice/keywordSlice";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(reducer, composeWithDevTools());
+const reducers = combineReducers(authReducer, keywordReducer);
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
 
 export default store;
