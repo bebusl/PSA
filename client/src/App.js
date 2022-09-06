1f8150dbee3a94fc996249d6d3284ee061c7bd07
bebusl <bebus1998@naver.com>
2022-09-06 22:37:55 +0900

    refactor: redux-persist 적용

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,19 @@ import "./styles/css/App.css";
 import io from "socket.io-client";
 import DefaultRoutes from "./routes/DefaultRoutes";
 import Content from "./components/layout/Content";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
+import store from "./redux/store";
 
 const socket = io.connect("http://localhost:5000");
+const persistor = persistStore(store);
 
 function App() {
   return (
     <Content>
-      <DefaultRoutes socket={socket} />
+      <PersistGate loading={null} persistor={persistor}>
+        <DefaultRoutes socket={socket} />
+      </PersistGate>
     </Content>
   );
 }
