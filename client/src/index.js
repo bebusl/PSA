72371b9a92ef298a7529c5f2a364a3b8d2569051
bebusl <bebus1998@naver.com>
2022-09-13 18:26:10 +0900

    feat: Global Style 지정

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import store from "./store/store";
 import "./styles/css/index.css";
 import App from "./App";
+import GlobalStyle from "./styles/globalStyle";
 
 const persistor = persistStore(store);
 
@@ -22,6 +23,7 @@ root.render(
   <BrowserRouter>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
+        <GlobalStyle />
         <App />
       </PersistGate>
     </Provider>
