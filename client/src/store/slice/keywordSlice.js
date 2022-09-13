3787c8399c33781670b857329ea7d4ce2638d4ec
bebusl <bebus1998@naver.com>
2022-09-14 00:57:36 +0900

    feat: 키워드 슬라이스에 키워드와 상품 정보 스테이트 생성

--- client/src/store/slice/keywordSlice.js
+++ client/src/store/slice/keywordSlice.js
@@ -2,8 +2,8 @@ import { createSlice } from "@reduxjs/toolkit";
 import socket from "../../socket";
 
 const initialState = {
-  preferKeyword: [],
-  dispreferKeyword: [],
+  preferKeywords: [],
+  dispreferKeywords: [],
   productLists: [],
   searchItem: "",
 };
@@ -12,21 +12,24 @@ export const keywordSlice = createSlice({
   name: "keyword",
   initialState: initialState,
   reducers: {
-    logoff: (state) => {
-      state = initialState;
-    },
-    updateLoginStatus: (state, action) => {
-      state.isLogin = true;
-      state.email = action.payload.email;
-      state.authToken = action.payload.auth_token;
-    },
     setSearchItem: (state, action) => {
       state.searchItem = action.payload;
       socket.emit("send message", { searchItem: action.payload });
     },
+    setKeywords: (state, action) => {
+      state[action.payload.type] = action.payload.keywords;
+    },
+    setProductList: (state, action) => {
+      state.productLists = action.payload;
+    },
   },
 });
 
-export const { logoff, updateLoginStatus, setSearchItem } =
-  keywordSlice.actions;
+export const {
+  logoff,
+  updateLoginStatus,
+  setSearchItem,
+  setKeywords,
+  setProductList,
+} = keywordSlice.actions;
 export default keywordSlice.reducer;
