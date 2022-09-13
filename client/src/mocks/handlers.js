a1152b6ac37de304e88deb23330003eecdb14e91
bebusl <bebus1998@naver.com>
2022-09-13 16:28:33 +0900

    feat: mock api 핸들러 작성

--- client/src/mocks/handlers.js
+++ client/src/mocks/handlers.js
@@ -5,7 +5,15 @@ import { rest } from "msw";
 
 export const handlers = [
   // Handles a POST /login request
-  rest.post("/login", (req, res, ctx) => {
+  rest.get("/auth/login", (req, res, ctx) => {
+    // Persist user's authentication in the session
+    sessionStorage.setItem("is-authenticated", "true");
+    return res(
+      // Respond with a 200 status code
+      ctx.status(200)
+    );
+  }),
+  rest.get("/auth/status", (req, res, ctx) => {
     // Persist user's authentication in the session
     sessionStorage.setItem("is-authenticated", "true");
     return res(
